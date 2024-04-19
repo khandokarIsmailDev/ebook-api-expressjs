ccab81294266b60183e72524808cba76da927300
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-20 04:37:24 +0600

    add config port in server.ts file

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import app from "./src/app";
-
-
+import { config } from "./config/config";
 
 
 
@@ -10,7 +9,7 @@ import app from "./src/app";
 
 
 const startServer = () =>{
-    const port = process.env.PORT || 3000
+    const port = config.port || 3000
 
     app.listen(port,()=>{
         console.log(`Listening port ${port}`)
