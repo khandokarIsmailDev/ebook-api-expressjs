c1d6fad204aa3051c0974fc27a4e02608f905c12
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-21 05:34:44 +0600

    add book api

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
+import bookRouter from './book/bookRouter';
 
 const app = express()
 
@@ -21,6 +22,9 @@ app.get('/',(req,res)=>{
 
 //  /api/users/        router
 app.use('/api/users',userRouter)
+
+//   /api/books/      router
+app.use('/api/books',bookRouter)
 
 
 //Global error handling
