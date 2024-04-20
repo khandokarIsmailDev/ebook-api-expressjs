b7449f87c0cb2131ea21683915b15f0a2778bbc2
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-04-20 18:57:23 +0600

    database call for find email in usercontroler

--- src/user/userControler.ts
+++ src/user/userControler.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModal from './userModal'
 
-
-const createUser = (req:Request,res:Response,next:NextFunction) =>{
+const createUser = async (req:Request,res:Response,next:NextFunction) =>{
 
     const {name,email,password} = req.body
 
     //validation
     if(!name || !email || !password){
         const error = createHttpError(400,"All field are required!")
+        return next(error)
+    }
+
+    //database call
+    const user = await userModal.findOne({email:email})
+
+    if(user){
+        const error = createHttpError(400,"user already exits with this email")
         return next(error)
     }
 
